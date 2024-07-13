29af42fd551a309d905cccf5ff8619a5b38e0526
Mihika Krishna <krishna.mihika@gmail.com>
2024-07-13 12:55:19 -0700

    Add AddExpensePage form

--- BalanceBuddyDesktop/App.axaml.cs
+++ BalanceBuddyDesktop/App.axaml.cs
@@ -3,6 +3,7 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
+using BalanceBuddyDesktop.Models;
 using BalanceBuddyDesktop.ViewModels;
 using BalanceBuddyDesktop.Views;
 
@@ -27,7 +28,7 @@ namespace BalanceBuddyDesktop
                     DataContext = new MainWindowViewModel(),
                 };
             }
-
+            var userData = GlobalData.Instance;
             base.OnFrameworkInitializationCompleted();
         }
     }
--- BalanceBuddyDesktop/ViewModels/AddExpensePageViewModel.cs
+++ BalanceBuddyDesktop/ViewModels/AddExpensePageViewModel.cs
@@ -1,6 +1,31 @@
-﻿namespace BalanceBuddyDesktop.ViewModels
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Input;
+using BalanceBuddyDesktop.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace BalanceBuddyDesktop.ViewModels
 {
-    public class AddExpensePageViewModel : ViewModelBase
+    public partial class AddExpensePageViewModel : ViewModelBase, INotifyPropertyChanged
     {
+        [ObservableProperty]
+        private Expense _newExpense = new Expense();
+
+        [ObservableProperty]
+        private List<ExpenseCategory> _categories = GlobalData.Instance.ExpenseCategories;
+
+        public AddExpensePageViewModel()
+        {
+
+        }
+
+        [RelayCommand]
+        private void AddExpense()
+        {
+            GlobalData.Instance.Expenses.Add(_newExpense);
+            _newExpense = new Expense();
+            OnPropertyChanged(nameof(_newExpense));
+        }
     }
 }
