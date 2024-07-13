e3dc0b3e537f23cab31ae663802991bc7460aff4
Mihika Krishna <krishna.mihika@gmail.com>
2024-07-12 20:35:53 -0700

    Add ItemTemplates for Sidebar ListItems

--- BalanceBuddyDesktop/ViewModels/MainWindowViewModel.cs
+++ BalanceBuddyDesktop/ViewModels/MainWindowViewModel.cs
@@ -1,22 +1,39 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
 
-namespace BalanceBuddyDesktop.ViewModels
+namespace BalanceBuddyDesktop.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase, INotifyPropertyChanged
 {
-    public partial class MainWindowViewModel : ViewModelBase, INotifyPropertyChanged
+    [ObservableProperty]
+    private bool _isPaneOpen = true;
+
+    [ObservableProperty]
+    private ViewModelBase _currentPage = new HomePageViewModel();
+
+    public ObservableCollection<ListItemTemplate> Items { get; } = new()
     {
-        [ObservableProperty]
-        private bool _isPaneOpen = true;
+        new ListItemTemplate(typeof(HomePageViewModel))
+    };
 
-        [ObservableProperty]
-        private ViewModelBase _currentPage = new HomePageViewModel();
-
-        [RelayCommand]
-        private void TriggerPane()
-        {
-            IsPaneOpen = !IsPaneOpen;
-        }
+    [RelayCommand]
+    private void TriggerPane()
+    {
+        IsPaneOpen = !IsPaneOpen;
     }
+}
+
+public class ListItemTemplate
+{
+    public ListItemTemplate(Type type) 
+    { 
+        ModelType = type;
+        Label = type.Name.Replace("PageViewModel", "");
+    }   
+    public string Label { get; }
+    public Type ModelType { get; }
 }
