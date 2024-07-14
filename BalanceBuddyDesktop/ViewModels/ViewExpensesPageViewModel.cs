8cf821cf28be123a666f1491097e62c1a2fd8d9e
Mihika Krishna <krishna.mihika@gmail.com>
2024-07-14 13:38:59 -0700

    Add basic chart

--- BalanceBuddyDesktop/ViewModels/ViewExpensesPageViewModel.cs
+++ BalanceBuddyDesktop/ViewModels/ViewExpensesPageViewModel.cs
@@ -1,6 +1,30 @@
-﻿namespace BalanceBuddyDesktop.ViewModels
+﻿using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using LiveChartsCore.SkiaSharpView.VisualElements;
+using SkiaSharp;
+
+
+namespace BalanceBuddyDesktop.ViewModels
 {
     public class ViewExpensesPageViewModel : ViewModelBase
     {
-    }
+        public ISeries[] Series { get; set; } =
+        {
+            new LineSeries<double>
+            {
+                Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
+                Fill = null
+            }
+        };
+
+        public LabelVisual Title { get; set; } =
+            new LabelVisual
+            {
+                Text = "My chart title",
+                TextSize = 25,
+                Padding = new LiveChartsCore.Drawing.Padding(15),
+                Paint = new SolidColorPaint(SKColors.DarkSlateGray)
+            };
+        }
 }
