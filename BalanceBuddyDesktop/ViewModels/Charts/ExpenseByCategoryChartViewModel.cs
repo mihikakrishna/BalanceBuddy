6640de8336d75bae58e4d062cfd03ae47c6d46ad
Mihika Krishna <krishna.mihika@gmail.com>
2024-07-14 14:07:49 -0700

    Add ExpenseByCategory Pie Chart

--- BalanceBuddyDesktop/ViewModels/Charts/ExpenseByCategoryChartViewModel.cs
+++ BalanceBuddyDesktop/ViewModels/Charts/ExpenseByCategoryChartViewModel.cs
@@ -1,4 +1,6 @@
-﻿using LiveChartsCore;
+﻿using System.Linq;
+using BalanceBuddyDesktop.Models;
+using LiveChartsCore;
 using LiveChartsCore.SkiaSharpView;
 using LiveChartsCore.SkiaSharpView.Painting;
 using LiveChartsCore.SkiaSharpView.VisualElements;
@@ -8,22 +10,33 @@ namespace BalanceBuddyDesktop.ViewModels.Charts
 {
     public class ExpenseByCategoryChartViewModel
     {
-        public ISeries[] Series { get; set; } =
-        {
-            new LineSeries<double>
-            {
-                Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
-                Fill = null
-            }
-        };
+        public ISeries[] Series { get; set; }
 
         public LabelVisual Title { get; set; } =
             new LabelVisual
             {
-                Text = "My chart title",
+                Text = "Total Expenses by Category",
                 TextSize = 25,
                 Padding = new LiveChartsCore.Drawing.Padding(15),
                 Paint = new SolidColorPaint(SKColors.DarkSlateGray)
             };
+
+        public ExpenseByCategoryChartViewModel()
+        {
+            var expenses = GlobalData.Instance.Expenses;
+            var totalByCategory = expenses
+                .GroupBy(e => e.Category.Name)
+                .Select(group => new
+                {
+                    CategoryName = group.Key,
+                    TotalAmount = group.Sum(e => e.Amount)
+                });
+
+            Series = totalByCategory.Select(category => new PieSeries<decimal>
+            {
+                Values = new decimal[] { category.TotalAmount },
+                Name = category.CategoryName
+            }).ToArray();
+        }
     }
 }
