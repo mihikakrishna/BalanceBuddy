9999ad4f5793cbd51350904557a7784eadc15218
Mihika Krishna <krishna.mihika@gmail.com>
2024-07-14 13:57:59 -0700

    Extract Chart into seperate ViewModel

---
+++ BalanceBuddyDesktop/ViewModels/Charts/ExpenseByCategoryChartViewModel.cs
@@ -0,0 +1,29 @@
+﻿using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using LiveChartsCore.SkiaSharpView.VisualElements;
+using SkiaSharp;
+
+namespace BalanceBuddyDesktop.ViewModels.Charts
+{
+    public class ExpenseByCategoryChartViewModel
+    {
+        public ISeries[] Series { get; set; } =
+        {
+            new LineSeries<double>
+            {
+                Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
+                Fill = null
+            }
+        };
+
+        public LabelVisual Title { get; set; } =
+            new LabelVisual
+            {
+                Text = "My chart title",
+                TextSize = 25,
+                Padding = new LiveChartsCore.Drawing.Padding(15),
+                Paint = new SolidColorPaint(SKColors.DarkSlateGray)
+            };
+    }
+}
--- BalanceBuddyDesktop/ViewModels/ViewExpensesPageViewModel.cs
+++ BalanceBuddyDesktop/ViewModels/ViewExpensesPageViewModel.cs
@@ -3,28 +3,12 @@ using LiveChartsCore.SkiaSharpView;
 using LiveChartsCore.SkiaSharpView.Painting;
 using LiveChartsCore.SkiaSharpView.VisualElements;
 using SkiaSharp;
+using BalanceBuddyDesktop.ViewModels.Charts;
 
 
-namespace BalanceBuddyDesktop.ViewModels
+namespace BalanceBuddyDesktop.ViewModels;
+
+public class ViewExpensesPageViewModel : ViewModelBase
 {
-    public class ViewExpensesPageViewModel : ViewModelBase
-    {
-        public ISeries[] Series { get; set; } =
-        {
-            new LineSeries<double>
-            {
-                Values = new double[] { 2, 1, 3, 5, 3, 4, 6 },
-                Fill = null
-            }
-        };
-
-        public LabelVisual Title { get; set; } =
-            new LabelVisual
-            {
-                Text = "My chart title",
-                TextSize = 25,
-                Padding = new LiveChartsCore.Drawing.Padding(15),
-                Paint = new SolidColorPaint(SKColors.DarkSlateGray)
-            };
-        }
+    public ExpenseByCategoryChartViewModel ExpenseByCategoryChartViewModel { get; } = new();
 }
