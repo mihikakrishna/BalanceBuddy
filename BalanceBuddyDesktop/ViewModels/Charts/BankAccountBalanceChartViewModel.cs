aacda5c2412d5645cf9747a97abf3f95f37de048
Mihika Krishna <krishna.mihika@gmail.com>
2024-08-18 17:12:20 -0500

    Add Bank Accounts Balance Chart

---
+++ BalanceBuddyDesktop/ViewModels/Charts/BankAccountBalanceChartViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using BalanceBuddyDesktop.Models;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using LiveChartsCore.SkiaSharpView.VisualElements;
+using SkiaSharp;
+
+namespace BalanceBuddyDesktop.ViewModels.Charts
+{
+    public class BankAccountBalanceChartViewModel
+    {
+        public ISeries[] Series { get; set; }
+
+        public LabelVisual Title { get; set; } =
+            new LabelVisual
+            {
+                Text = "Bank Account Balances",
+                TextSize = 25,
+                Padding = new LiveChartsCore.Drawing.Padding(15),
+                Paint = new SolidColorPaint(SKColors.DarkSlateGray)
+            };
+
+        public BankAccountBalanceChartViewModel()
+        {
+            var bankAccounts = GlobalData.Instance.BankAccounts; // Assuming GlobalData contains a list of bank accounts
+            var totalByAccount = bankAccounts
+                .Select(account => new
+                {
+                    AccountName = account.Name,
+                    Balance = account.Balance
+                });
+
+            Series = totalByAccount.Select(account => new PieSeries<decimal>
+            {
+                Values = new decimal[] { account.Balance },
+                Name = account.AccountName
+            }).ToArray();
+        }
+    }
+}
--- BalanceBuddyDesktop/ViewModels/ViewExpensesPageViewModel.cs
+++ BalanceBuddyDesktop/ViewModels/ViewExpensesPageViewModel.cs
@@ -12,4 +12,5 @@ public class ViewExpensesPageViewModel : ViewModelBase
 {
     public ExpenseByCategoryChartViewModel ExpenseByCategoryChartViewModel { get; } = new();
     public StackedTransactionChartViewModel StackedTransactionChartViewModel { get; } = new();
+    public BankAccountBalanceChartViewModel BankAccountBalanceChartViewModel { get; } = new();
 }
