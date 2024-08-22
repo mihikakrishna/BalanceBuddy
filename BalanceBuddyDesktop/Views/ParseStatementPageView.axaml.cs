f4d99b9a425a1c62abd7b17d71db8ae33d9aa0cd
Mihika Krishna <krishna.mihika@gmail.com>
2024-08-22 16:19:50 -0700

    Add new page

---
+++ BalanceBuddyDesktop/Views/ParseStatementPageView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace BalanceBuddyDesktop.Views;
+
+public partial class ParseStatementPageView : UserControl
+{
+    public ParseStatementPageView()
+    {
+        InitializeComponent();
+    }
+}
