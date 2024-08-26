4ae03496dcbdef021e28e11b227b3f3e8f49a91d
Mihika Krishna <krishna.mihika@gmail.com>
2024-08-26 14:26:33 -0700

    Make UI more intuitive

--- BalanceBuddyDesktop/Views/ParseStatementPageView.axaml.cs
+++ BalanceBuddyDesktop/Views/ParseStatementPageView.axaml.cs
@@ -1,4 +1,7 @@
+using System.IO;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
 
 namespace BalanceBuddyDesktop.Views;
 
@@ -7,5 +10,32 @@ public partial class ParseStatementPageView : UserControl
     public ParseStatementPageView()
     {
         InitializeComponent();
+    }
+
+    private async void OpenFileButton_Clicked(object sender, RoutedEventArgs args)
+    {
+        var topLevel = TopLevel.GetTopLevel(this);
+        var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        {
+            Title = "Open Text File",
+            AllowMultiple = false
+        });
+
+        if (files.Count >= 1)
+        {
+            await using var stream = await files[0].OpenReadAsync();
+            using var streamReader = new StreamReader(stream);
+            var fileContent = await streamReader.ReadToEndAsync();
+            SelectedFileName.Text = files[0].Name; // Update the label with the selected file's name
+        }
+        else
+        {
+            SelectedFileName.Text = "No file selected";
+        }
+    }
+
+    private void ParseFileButton_Clicked(object sender, RoutedEventArgs args)
+    {
+        // Your logic to parse the selected file goes here
     }
 }
