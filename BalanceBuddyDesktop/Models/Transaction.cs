fb277b24a32f1f92c014864a04f83c20685edde7
Mihika Krishna <krishna.mihika@gmail.com>
2024-09-14 09:26:16 -0700

    Cleaning up transactions

---
+++ BalanceBuddyDesktop/Models/Transaction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BalanceBuddyDesktop.Models;
+
+namespace BalanceBuddyDesktop.Models
+{
+    public abstract class Transaction
+    {
+        public string Id { get; set; }
+        public bool IsSelected { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string FormattedDate => Date.ToString("d");
+        public string Description { get; set; }
+    }
+}
+
+public class Expense : Transaction
+{
+    public ExpenseCategory Category { get; set; }
+}
+
+public class Income : Transaction
+{
+    public IncomeCategory Category { get; set; }
+}
