40303e302d6a607cfe027a78ce732d8c1c249862
Mihika Krishna <krishna.mihika@gmail.com>
2025-02-13 20:15:36 -0800

    Add basic test framework. Add unit tests and functional tests for AddTransactionPage

---
+++ BalanceBuddyDesktop.Tests/Unit/AddTransactionPageViewModelTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using NUnit.Framework;
+using BalanceBuddyDesktop.ViewModels;
+using BalanceBuddyDesktop.Models;
+
+namespace BalanceBuddyDesktop.Tests.Unit
+{
+    [TestFixture]
+    public class AddTransactionPageViewModelTests
+    {
+        private AddTransactionPageViewModel _viewModel;
+
+        [SetUp]
+        public void Setup()
+        {
+            _viewModel = new AddTransactionPageViewModel();
+        }
+
+        [Test]
+        public void AddExpense_ShouldIncreaseExpenseCount()
+        {
+            var expense = new Expense { Amount = 100, Date = DateTime.Now, Description = "Test Expense" };
+            _viewModel.NewExpense = expense;
+
+            _viewModel.AddExpenseCommand.Execute(null);
+
+            Assert.That(_viewModel.Expenses.Contains(expense), Is.True);
+            Assert.That(_viewModel.Expenses.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void DeleteExpense_ShouldDecreaseExpenseCount()
+        {
+            var expense = new Expense { Amount = 100, Date = DateTime.Now, Description = "Test Expense" };
+            _viewModel.Expenses.Add(expense);
+
+            _viewModel.DeleteExpenseCommand.Execute(expense);
+
+            Assert.That(_viewModel.Expenses.Contains(expense), Is.False);
+        }
+
+        // Similar tests for Income and BankAccount should be added here
+    }
+}
