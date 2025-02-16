ff8c2a9864041d716defed9a640d0a5d80db3b71
Mihika Krishna <krishna.mihika@gmail.com>
2025-02-16 11:51:54 -0800

    Highlight unreviewed category cell

--- BalanceBuddyDesktop/Models/IncomeCategory.cs
+++ BalanceBuddyDesktop/Models/IncomeCategory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using Avalonia.Media;
 
 namespace BalanceBuddyDesktop.Models;
 
@@ -7,26 +8,36 @@ public class IncomeCategory : INotifyPropertyChanged
 {
     public int Id { get; set; }
     public bool IsSelected { get; set; }
+
     public event PropertyChangedEventHandler PropertyChanged;
 
     protected virtual void OnPropertyChanged(string propertyName)
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
+
     private string _name;
     public string Name
     {
         get => _name;
         set
         {
-            if (string.Equals(_name, "Unreviewed", StringComparison.OrdinalIgnoreCase))
-                return;
-
             if (_name != value)
             {
                 _name = value;
                 OnPropertyChanged(nameof(Name));
+                OnPropertyChanged(nameof(CellColor));
             }
+        }
+    }
+
+    public IBrush CellColor
+    {
+        get
+        {
+            return string.Equals(Name, "Unreviewed", StringComparison.OrdinalIgnoreCase)
+                ? Brushes.RosyBrown
+                : Brushes.Transparent;
         }
     }
 }
